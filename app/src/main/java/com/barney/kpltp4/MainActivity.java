cec1d00e4a5263bcb9fafda50ed85c5ab26fd030
Howler Nouvelle <hilmy@wahidin.net>
2022-03-17 23:29:25 +0700

    benerin conflict

--- app/src/main/java/com/barney/kpltp4/MainActivity.java
+++ app/src/main/java/com/barney/kpltp4/MainActivity.java
@@ -3,6 +3,7 @@ package com.barney.kpltp4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,13 +13,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView miomio = findViewById(R.id.miomio);
+        TextView miomio2 = findViewById(R.id.miomio2);
+
         HaloGeneric hutao = new HaloGeneric(miomio);
 
         //set inputan yg di soal nama nya X
         hutao.SapaUser("yeriko makici");
 
-        TextView miomio = findViewById(R.id.miomio);
-        DataGeneric y = new DataGeneric("1302204094", miomio);
+        DataGeneric y = new DataGeneric("1302204094", miomio2);
 
         y.PrintData();
     }
